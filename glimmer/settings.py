b606c30f5658763d01e6535726f4a4d92d4ce0e0
tomik-z-cech <tkubancik@gmail.com>
2023-11-20 15:21:56 +0000

    Revert "Add heroku url to allowed hosts"
    This reverts commit ef6a87263de113026a4b42c0f4938777670382e4.

--- glimmer/settings.py
+++ glimmer/settings.py
@@ -31,8 +31,7 @@ DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = [
     '8000-tomik-z-cech-pp4-aneta-s-da30e342lk.us2.codeanyapp.com',
-    'localhost',
-    'anetas-glimmer-05b4a3ceb113.herokuapp.com'
+    'localhost'
 ]
 
 
