6443b030624a07e8dacf026b3d34f62e36028978
tomik-z-cech <tkubancik@gmail.com>
2023-11-20 19:34:08 +0000

    Add "base" view

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.views import generic, View
+from django.contrib import messages
+from django.http import HttpResponseRedirect
 
-# Create your views here.
+class Home(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "base.html",
+        )
+
--- glimmer/urls.py
+++ glimmer/urls.py
@@ -5,7 +5,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path("", include("bookings.urls"), name="bookings-urls"),
+    path("", include("bookings.urls"), name="bookings-urls"),
     path('summernote/', include('django_summernote.urls')),
-    # path('accounts/', include('allauth.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
