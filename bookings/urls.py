06758c7d0aba7c6111dc8b441d816e3f8f0a85dd
tomik-z-cech <tkubancik@gmail.com>
2023-11-21 12:16:39 +0000

    Create logic for home page

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.Home.as_view(), name='home'),
+    path('', views.NewsList.as_view(), name='home'),
     ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic, View
-from django.contrib import messages
-from django.http import HttpResponseRedirect
+from django.views import generic,View
+from .models import NewsPosts
 
-class Home(View):
-    def get(self, request, *args, **kwargs):
-        return render(
-            request,
-            "index.html",
-        )
+class NewsList(generic.ListView):
+    model = NewsPosts
+    result = NewsPosts.objects.filter(is_published=1).order_by('-created_on')
+    template_name = 'index.html'
+    context_object_name = 'news_list'
 
