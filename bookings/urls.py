54f5543d2c123c1ccb0ec7256e7549c1238eb77b
tomik-z-cech <tkubancik@gmail.com>
2023-11-21 18:29:14 +0000

    Add news detail view

--- bookings/urls.py
+++ bookings/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.NewsList.as_view(), name='home'),
+    path('news/<slug:slug>/', views.NewsDetailView.as_view(), name='news-detail'),
     ]
--- bookings/views.py
+++ bookings/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, get_object_or_404, reverse
-from django.views import generic,View
+from django.views import generic
 from .models import NewsPosts
 
 class NewsList(generic.ListView):
     model = NewsPosts
-    result = NewsPosts.objects.filter(is_published=1).order_by('-created_on')
+    queryset = NewsPosts.objects.filter(is_published=1).order_by('-created_on')
     template_name = 'index.html'
     context_object_name = 'news_list'
+
+class NewsDetailView(generic.DetailView):
+    model = NewsPosts
+    slug_field = 'slug'
+    template_name = 'news_detail.html'
+    context_object_name = 'news_detail'
