a3a34d578d534dc664551f04f09b0554b0b10c1e
tomik-z-cech <tkubancik@gmail.com>
2023-12-10 13:24:53 +0000

    Fix bug with count of not approved comments

--- landing/views.py
+++ landing/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.db.models import Count
+from django.db.models import Count, Q
 from news.models import NewsPosts
 from styles.models import StylesAvailable
 from artists.models import Artists
@@ -12,7 +12,7 @@ class LandingPageView(generic.ListView):
     """
     template_name = 'landing/index.html'
     def get(self, request, *args, **kwargs):
-        news = NewsPosts.objects.annotate(comments_num=Count('news_comments')).filter(is_published=1)[:3]
+        news = NewsPosts.objects.annotate(comments_num=Count('news_comments', filter=Q(news_comments__approved=1))).filter(is_published=1)[:3]
         top_styles_like = StylesAvailable.objects.annotate(likes_num=Count('likes')).order_by('-likes_num')[:3]
         top_styles_try = StylesAvailable.objects.annotate(want_to_try_num=Count('want_to_try')).order_by('-want_to_try_num')[:3]
         top_artists = Artists.objects.all().order_by('-rating')[:3]
