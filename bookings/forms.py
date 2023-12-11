9d9ce07dccc0a97483cc8d0a9b76fd0588f00db4
tomik-z-cech <tkubancik@gmail.com>
2023-12-11 19:48:48 +0000

    Create NewBooking form

--- bookings/forms.py
+++ bookings/forms.py
@@ -1,21 +1,46 @@
-from django.shortcuts import redirect
 from django import forms
+from django.utils import timezone
+from datetime import datetime, timedelta
 from bookings.models import Bookings
-
-
-
-
 
 class CreateBookingForm(forms.ModelForm):
     class Meta:
         model = Bookings
-        fields = ['date_time']
+        fields = (
+            "date",
+            "time",
+            "booked_artist",
+            "booked_style"
+        )
 
-    your_datetime_field = forms.SplitDateTimeField(
-        label='Date and Time',
-        widget=forms.SplitDateTimeWidget(
-            date_attrs={'type': 'date'},
-            time_attrs={'type': 'time'},
-        ),
-        hours=range(9, 16)
+    date = forms.DateField(
+        label="Date",
+        widget=forms.TextInput(attrs={"type": "date"}),
+        help_text="* Please select date of your visit",
     )
+
+    time = forms.ChoiceField(
+        label="Time",
+        choices=[(f"{h:02d}:00", f"{h:02d}:00") for h in range(9, 17, 1)],
+        widget=forms.Select,
+        help_text="* Please select time of your visit",
+    )
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        min_extra_days = 2
+        min_date = timezone.now() + timedelta(days=min_extra_days)
+        self.fields['date'].widget.attrs.update({'min': min_date.date()})
+
+    def clean(self):
+        cleaned_data = super().clean()
+        date = cleaned_data.get('date')
+        time = cleaned_data.get('time')
+
+        # Combine date and time to create date_time
+        if date and time:
+            date_time = datetime.combine(date, time)
+            cleaned_data["date_time"] = timezone.make_aware(date_time)
+
+        return cleaned_data
+    
--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from bookings.models import Bookings
+from bookings.forms import CreateBookingForm
 
 
 class MyBookingsView(generic.ListView):
@@ -17,18 +18,20 @@ class MyBookingsView(generic.ListView):
                 "bookings": user_bookings,
             },
         )
-        
+
+
 class NewBookingView(generic.ListView):
     template_name = "bookings/new_booking.html"
     model = Bookings
-    
+
     def get(self, request, *args, **kwargs):
-        login_user = request.user
-        user_bookings = Bookings.objects.filter(username=login_user)
         return render(
             request,
             self.template_name,
             {
-                "new_booking_form": user_bookings,
+                "new_booking_form": CreateBookingForm(),
             },
         )
+
+    def post(self, request, *args, **kwargs):
+        return redirect("my-bookings")
