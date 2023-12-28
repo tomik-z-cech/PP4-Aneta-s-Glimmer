c41c98ae1cdda33f7cbf475786fe01d9bf521b0d
tomik-z-cech <tkubancik@gmail.com>
2023-12-28 14:48:07 +0000

    Add  approve multiple comms func - styles

--- styles/admin.py
+++ styles/admin.py
@@ -8,6 +8,12 @@ from styles.models import StylesAvailable, StylesComments
 @admin.register(StylesComments)
 class StylesCommentAdmin(admin.ModelAdmin):
     list_display = ("creator", "comment_body", "style", "approved")
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+    approve_comments.short_description = "Approve selected comments"
 
 
 @admin.register(StylesAvailable)
