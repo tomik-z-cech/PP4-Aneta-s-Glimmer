140559cbf3d082d025c9bb351a8028bafc5fa0ec
tomik-z-cech <tkubancik@gmail.com>
2023-12-29 17:01:58 +0000

    Add missing aria-label to search bar

--- landing/forms.py
+++ landing/forms.py
@@ -4,7 +4,10 @@ from django import forms
 
 
 class SearchBarForm(forms.Form):
-    search_query = forms.CharField(max_length=255, required=True)
+    search_query = forms.CharField(
+        max_length=255,
+        required=True,
+        widget=forms.TextInput(attrs={'aria-label': 'Search Bar'}))
 
     class Meta:
         fields = ("search_query",)
